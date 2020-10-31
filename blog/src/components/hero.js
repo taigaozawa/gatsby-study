7b72b870f67fa835f743abcae7fcaae8c255bf10
taigaozawa <taigaozawa@gmail.com>
2020-11-01 04:27:14 +0900

    Gatsby勉強
    環境変数を分離

---
+++ blog/src/components/hero.js
@@ -0,0 +1,22 @@
+import React from "react";
+import '../styles/hero.css';
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h1 className="hero-text">
+        Welcome to my blog! <br />
+        Enjoy lots of posts. <br />
+      </h1>
+      <div className="hero-author">
+        <img src="https://api.adorable.io/avatars/30/iamge.png" className="hero-author-image" alt="avatar"></img>
+        <p className="hero-author-text">
+          Written by XXXX. <br />
+          Front Engineer at Hoge.Inc. Love JavaScript.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Hero;
